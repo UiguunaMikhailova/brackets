20665a445e750735811b0d4658e76b050d26d55b
UiguunaMikhailova <uyguuna1997@yandex.ru>
2022-02-21 18:53:57 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) return false
+  const stack = []
+
+  for (let brackets of bracketsConfig) {
+    for (const symbol of str) {
+    if (symbol === brackets[0]) {
+      stack.push(symbol);
+    } else if (symbol === brackets[1]) {
+      if (!stack.pop()) {
+        return false;
+      }
+    }
+  }
+  }
+  return stack.length === 0;
 }
